bd7ec37a9859eaebf1cf6581a9f740c8e1cf42bd
Nialv <sronnieb@gmail.com>
2023-12-22 14:13:59 -0600

    backend tasks crud

--- time-tracker-api/src/routes/protectedRoutes.ts
+++ time-tracker-api/src/routes/protectedRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/jwtUtils';
+import { createTaskController, findTaskByIdController, findTasksController, modifyTaskController, removeTaskController } from '../controllers/taskController';
 
 const router = express.Router();
 
-router.get('/protected', verifyToken, (req, res) => {
-  // Handle the protected route
-  res.send('This is a protected route');
-});
+router.post('/tasks', verifyToken, createTaskController);
+router.put('/tasks/:id', verifyToken, modifyTaskController);
+router.get('/tasks', verifyToken, findTasksController);
+router.get('/tasks/:id', verifyToken, findTaskByIdController);
+router.delete('/tasks/:id', verifyToken, removeTaskController);
 
 export default router;
